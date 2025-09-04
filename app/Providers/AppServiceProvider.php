695855cee6c450d6b0307c9fb33cfbb8ecb59ead
abdokg4 <abdokg4@gmail.com>
2025-09-05 02:21:23 +0300

    updating for vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+
     }
 
     /**
@@ -21,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+        if (str_ends_with(request()->getHost(), '.ngrok-free.app')) {
+            URL::forceScheme('https');
+        }
     }
 }
